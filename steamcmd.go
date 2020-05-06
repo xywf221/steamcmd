011254a573c7b4cb224fa3d7dd819321fbd9aed6
吊塔 <993651481@qq.com>
2020-05-07 02:04:36 +0800

    'x'

---
+++ steamcmd.go
@@ -0,0 +1,97 @@
+package steamcmd
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+type SteamCmd struct {
+	cmd               *exec.Cmd
+	Available, Active bool
+	pipe              *bufio.ReadWriter
+}
+
+func NewSteamCmd(root string) (*SteamCmd, error) {
+	cmd := exec.Command("bash", root, "+login", "anonymous")
+	cmd.Stderr = os.Stderr
+	pr, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+	wc, err := cmd.StdinPipe()
+	if err != nil {
+		return nil, err
+	}
+	pipe := bufio.NewReadWriter(bufio.NewReader(pr), bufio.NewWriter(wc))
+	return &SteamCmd{cmd: cmd, pipe: pipe, Available: false, Active: false}, nil
+}
+
+func (s *SteamCmd) Run() error {
+	go s.cmd.Run()
+	for {
+		line, err := s.pipe.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				continue
+			}
+			return err
+		}
+		if strings.Contains(line, "Waiting for user info...OK") {
+			s.Available = true
+			return nil
+		}
+	}
+}
+
+func (s *SteamCmd) RunScript(script string) (result []byte, err error) {
+	if !s.Available && !s.Active {
+		err = errors.New("service not available")
+	}
+	log.Println("run script : " + script)
+	if _, err = s.pipe.WriteString(script + "\n"); err != nil {
+		return
+	}
+	if err = s.pipe.Flush(); err != nil {
+		return
+	}
+	s.Active = true
+
+	defer func() {
+		s.Active = false
+	}()
+
+	var token = 0
+	for {
+		line, _, err := s.pipe.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				continue
+			}
+			return nil, err
+		}
+		if token == 1 {
+			result = line
+			break
+		}
+		if strings.Contains(string(line), "Steam>") {
+			token++
+		}
+	}
+	return
+}
+
+func (s *SteamCmd) Close() error {
+	if s.Available && !s.Active {
+		_, err := s.RunScript("quit")
+		return err
+	} else {
+		if s.cmd.Process != nil {
+			return s.cmd.Process.Kill()
+		}
+	}
+	return nil
+}
---
+++ workshop_downloader.go
@@ -0,0 +1,105 @@
+package steamcmd
+
+import (
+	"archive/zip"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type WorkshopDownloader struct {
+	pool                      pool.Pool
+	steamCmdPath, ArchivePath string
+}
+
+func NewWorkshopDownloader(pSize int, scPath string) (*WorkshopDownloader, error) {
+	poolConfig := &pool.Config{
+		InitialCap: pSize, //资源池初始连接数
+		MaxIdle:    pSize, //最大空闲连接数
+		MaxCap:     pSize, //最大并发连接数
+		Factory: func() (interface{}, error) {
+			cmd, err := NewSteamCmd(scPath + "/steamcmd.sh")
+			if err != nil {
+				return nil, err
+			}
+			if err = cmd.Run(); err != nil {
+				return nil, err
+			}
+			return cmd, nil
+		},
+		Close: func(i interface{}) error {
+			return i.(*SteamCmd).Close()
+		},
+
+		//连接最大空闲时间，超过该时间的连接 将会关闭，可避免空闲时连接EOF，自动失效的问题
+		//IdleTimeout: 15 * time.Second,
+	}
+	p, err := pool.NewChannelPool(poolConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return &WorkshopDownloader{pool: p, steamCmdPath: scPath}, nil
+}
+
+func (w *WorkshopDownloader) Cache(appId, publishedFileId string) (string, error) {
+	script := fmt.Sprintf("workshop_download_item %s %s", appId, publishedFileId)
+	result, err := w.RunScript(script)
+	if err != nil {
+		return "", err
+	}
+	log.Println("script result: " + result)
+	if strings.Contains(result, "ERROR!") || !strings.Contains(result, "Success") {
+		//下载失败
+		return "", errors.New(result)
+	}
+	pos1 := strings.IndexByte(result, '"')
+	pos2 := strings.LastIndexByte(result, '"')
+	itemPath := result[pos1+1 : pos2]
+	appPath := w.steamCmdPath + "/steamapps/workshop/content/" + appId + "/"
+	filename := fmt.Sprintf("%s/%s.zip", w.ArchivePath, publishedFileId)
+	file, err := os.Create(filename)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+	archive := zip.NewWriter(file)
+	defer archive.Close()
+	archive.SetComment("archive create by 993651481@qq.com ")
+
+	err = filepath.Walk(itemPath, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		fn := strings.Replace(path, appPath, "", -1)
+		w, err := archive.Create(fn)
+		if err != nil {
+			return err
+		}
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		_, err = io.Copy(w, f)
+		return err
+	})
+	return filename, err
+}
+
+func (w *WorkshopDownloader) RunScript(script string) (string, error) {
+	p, err := w.pool.Get()
+	if err != nil {
+		return "", err
+	}
+	cmd := p.(*SteamCmd)
+	defer w.pool.Put(p)
+	result, err := cmd.RunScript(script)
+	if err != nil {
+		return "", err
+	}
+	return string(result), nil
+}
